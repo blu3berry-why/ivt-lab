0b145edfb0fa45a2791ffd79db14d12e76f84f64
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2022-04-22 13:32:03 +0200

    exception throw correction

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,7 +30,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
@@ -46,7 +46,6 @@ public class TorpedoStore {
     } else {
       // simulated failure
       success = false;
-    }
 
     return success;
   }
